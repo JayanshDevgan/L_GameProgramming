0f3d9952f2507248d80df09203d0c471655f4788
Jayansh Devgan <72650671+JayanshDevgan@users.noreply.github.com>
2025-03-18 00:46:15 +0530

    Almost done just stuck with one error

--- RPGCharacterSystem.cpp
+++ RPGCharacterSystem.cpp
@@ -11,12 +11,12 @@ private:
 public:
 	Weapon(int damage, int durability, std::string special_ability) :
 		m_damage(damage), m_durability(durability), m_special_ability(special_ability) {}
+
+	void Use() const { std::cout << "Using " << m_special_ability << "(Damage: " << m_damage << ", " << "Durability: " << m_durability << ")\n"; }
+	int GetDamage() const { return m_damage; }
 };
 
-class Inventory
-{
-	// TODO
-};
+class Party;
 
 class Character
 {
@@ -24,14 +24,29 @@ private:
 	std::string m_name;
 	int m_level, m_health, m_mana;
 
-	std::shared_ptr<Inventory> m_inventory = std::make_shared<Inventory>();
-	//std::unique_ptr<Weapon> m_weapon = std::make_unique<Weapon>();
+	std::unique_ptr<Weapon> m_weapon;
+	std::vector<std::shared_ptr<Character>> m_inventory;
+	std::shared_ptr<Party> m_party;
 
 public:
-	Character(std::string name, int level, int health, int mana) :
-		m_name(name), m_level(level), m_health(health), m_mana(mana) {}
+	Character(std::string name, int level, int health, int mana, std::shared_ptr<Party> party) :
+		m_name(name), m_level(level), m_health(health), m_mana(mana), m_party(party) {}
+
+	std::weak_ptr<Character> m_teammates;
 
 	std::string GetName() { return m_name; }
+
+	void SetWeapon(std::unique_ptr<Weapon> w) { m_weapon = std::move(w); }
+	void Attack(std::shared_ptr<Character> player) {
+		if (player != nullptr)
+		{
+			if (m_party->GetPlayerPartyIDByName(player->GetName()) != m_party->GetPlayerPartyIDByName(this->GetName()))
+			{
+				if (m_weapon) { m_weapon->Use(); player->m_health -= m_weapon->GetDamage(); }
+				else std::cout << m_name << " has no weapon!" << std::endl;
+			}
+		}
+	}
 };
 
 class Party
@@ -39,8 +54,7 @@ class Party
 private:
 	int m_Id, m_max_players;
 	std::string m_name;
-	std::weak_ptr<Character*> m_player;
-	std::vector<Character*> m_players;
+	std::vector<std::shared_ptr<Character>> m_players;
 
 public:
 	Party(int id, std::string name, int max_players) :
@@ -48,7 +62,7 @@ public:
 
 	void Show()
 	{
-		for (Character* player : m_players)
+		for (std::shared_ptr<Character> player : m_players)
 		{
 			std::cout << "Party ID -> " << m_Id << " Name -> "
 				<< m_name << " Player Name -> " << player->GetName()
@@ -57,41 +71,93 @@ public:
 		}
 	}
 
-	void Join(Character* player)
+	int GetPlayersSize() { return m_players.size(); }
+
+	void Join(std::shared_ptr<Character> player)
 	{
-		m_players.push_back(player);
+		if (GetPlayersSize() < m_max_players)
+			m_players.push_back(player);
+		else std::cout << "Party is full" << std::endl;
+	}
+
+	int GetPlayerPartyIDByName(std::string name)
+	{
+		for (const std::shared_ptr<Character>& player : m_players)
+		{
+			if (player->GetName() == name) {
+				return m_Id;
+			}
+		}
+		return -1;
 	}
 };
 
 int main()
 {
-	Character* alex = new Character("ALEX", 5, 100, 80);
-	Character* brody = new Character("BRODY", 5, 100, 90);
-	Character* chimcandy = new Character("CHIMCANDY", 7, 100, 30);
-	Character* bruno = new Character("BRUNO", 3, 100, 100);
-	Character* hex = new Character("HEX", 5, 100, 10);
-	Character* crokx = new Character("CROKX", 5, 130, 30);
-	Character* dg = new Character("DG", 5, 20, 100);
-	Character* nibin = new Character("NIBIN", 5, 30, 100);
-	Character* chimu = new Character("CHIMU", 2, 100, 10);
+	std::shared_ptr<Party> alpha_party = std::make_shared<Party>(114, "ALPHA", 5);
+	std::shared_ptr<Party> bravo_party = std::make_shared<Party>(2432, "BRAVO", 3);
+	std::shared_ptr<Party> champions_party = std::make_shared<Party>(32, "CHAMPIONS", 6);
 
-	Party* alpha_party = new Party(114, "ALPHA", 5);
-	Party* bravo_party = new Party(2432, "BRAVO", 6);
-	Party* champions_party = new Party(32, "CHAMPIONS", 6);
+	std::shared_ptr<Character> alex = std::make_shared<Character>("ALEX", 5, 100, 80, alpha_party);
+	alex->SetWeapon(std::make_unique<Weapon>(30, 100, "Sword"));
+
+	std::shared_ptr<Character> brody = std::make_shared<Character>("BRODY", 5, 100, 90, bravo_party);
+	brody->SetWeapon(std::make_unique<Weapon>(23, 100, "Knife"));
+
+	std::shared_ptr<Character> chimcandy = std::make_shared<Character>("CHIMCANDY", 7, 100, 30, champions_party);
+	chimcandy->SetWeapon(std::make_unique<Weapon>(30, 141, "AK47"));
+	
+	std::shared_ptr<Character> bruno = std::make_shared<Character>("BRUNO", 3, 100, 100, alpha_party);
+	bruno->SetWeapon(std::make_unique<Weapon>(32, 100, "M416"));
+
+	std::shared_ptr<Character> hex = std::make_shared<Character>("HEX", 5, 100, 10, alpha_party);
+	hex->SetWeapon(std::make_unique<Weapon>(23, 100, "Bazuka"));
+
+	std::shared_ptr<Character> crokx = std::make_shared<Character>("CROKX", 5, 130, 30, alpha_party);
+	crokx->SetWeapon(std::make_unique<Weapon>(64, 10, "Axe"));
+	
+	std::shared_ptr<Character> dg = std::make_shared<Character>("DG", 5, 20, 100, bravo_party);
+	dg->SetWeapon(std::make_unique<Weapon>(5, 100, "Spear"));
+	
+	std::shared_ptr<Character> nibin = std::make_shared<Character>("NIBIN", 5, 30, 100, bravo_party);
+	nibin->SetWeapon(std::make_unique<Weapon>(52, 100, "Bow&Arrow"));
+	
+	std::shared_ptr<Character> chimu = std::make_shared<Character>("CHIMU", 2, 100, 10, bravo_party);
+	chimu->SetWeapon(std::make_unique<Weapon>(45, 100, "Wipe"));
 
 	alpha_party->Join(alex);
 	alpha_party->Join(bruno);
 	alpha_party->Join(hex);
 	alpha_party->Join(crokx);
 
+	alex->m_teammates = bruno;
+	bruno->m_teammates = hex;
+	hex->m_teammates = crokx;
+	crokx->m_teammates = alex;
+
 	bravo_party->Join(brody);
 	bravo_party->Join(dg);
 	bravo_party->Join(nibin);
 	bravo_party->Join(chimu);
+
+	brody->m_teammates = dg;
+	dg->m_teammates = nibin;
+	nibin->m_teammates = chimu;
+	chimu->m_teammates = brody;
 
 	champions_party->Join(chimcandy);
 
 	alpha_party->Show();
 	bravo_party->Show();
 	champions_party->Show();
+
+	alex->Attack(brody);
+	brody->Attack(alex);
+	chimcandy->Attack(nullptr);
+	bruno->Attack(dg);
+	hex->Attack(chimu);
+	crokx->Attack(nibin);
+	dg->Attack(alex);
+	nibin->Attack(hex);
+	chimu->Attack(crokx);
 }
